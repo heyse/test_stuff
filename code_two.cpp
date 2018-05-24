c632169b489ea1ed27d7deca9f899f00609078ac
Jan Felix Heyse <heyse@DN0a625424.SUNet>
2018-05-24 13:46:56 -0700

    added second code with integer calculation

---
+++ code_two.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+
+int main() {
+
+  int a = 5;
+  int b = 6;
+  int c = a+b;
+
+  std::cout << "Hello" << std::endl;
+  std::cout << a << " + " << b << " = " << c << std::endl;
+
+
+  return 0;
+}
